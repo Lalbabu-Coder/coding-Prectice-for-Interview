e4c206741173a40c03f0182fa03fb1679ec6628c
Lalbabu-Coder <rishisingh765453@gmail.com>
2026-09-05 21:16:42 +0530

    Initial commit

---
+++ Day-1/index.js
@@ -0,0 +1,39 @@
+//print the value 0 to 10 ;
+
+
+// const val = 10;
+// for(i = 0; i<val ; i++){
+//     console.log(i);
+// }
+
+//create a function and sum of the all array
+
+// function sum(arr){
+//     let total = 0;
+//     for(let i = 0; i <arr.length ; i++){
+//         total += arr[i];
+//     }
+//         return total;
+    
+// }
+// console.log(sum([10,20,19,10,19]));
+
+
+// function sum(arr){
+//     let total = 0;
+//     for(let i = 0; i < arr.length; i++){
+//         total += arr[i];
+//     }
+//     return total;
+// }
+
+// console.log(sum([10,20,30,10]));
+
+// function sum(arr){
+//   let total = 0;
+//   for(let i = 0; i < arr.length ; i ++){
+//     total += arr[i];
+//   }
+//     return total;
+// }
+// console.log(sum([10,29,29,92,9]));
