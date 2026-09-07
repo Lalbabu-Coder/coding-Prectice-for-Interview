ba3b0fbfaded770c7d5f3412a44c8da1aca95f89
Lalbabu-Coder <rishisingh765453@gmail.com>
2026-09-07 13:53:42 +0530

    DAy-02-prectice

--- Day-2/index.js
+++ Day-2/index.js
@@ -16,15 +16,56 @@
 // console.log(secondmax);
 
 
-let arr = [10,12,29,48,90]
-let first=0;
-let sclargest=Infinity-1;
-for(let i = 0; i < arr.length; i++){
-    if(arr[i] > first){
-        sclargest=first
-        first=arr[i];
-    }else if(arr[i] > sclargest && arr[i] !== first){
-        sclargest=arr[i];
+// let arr = [10,12,29,48,90]
+// let first=0;
+// let sclargest=Infinity-1;
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] > first){
+//         sclargest=first
+//         first=arr[i];
+//     }else if(arr[i] > sclargest && arr[i] !== first){
+//         sclargest=arr[i];
+//     }
+// }
+// console.log(sclargest);
+
+// let arr = [10,10,38,28,19,38,67]
+// let first = 0;
+// let seclast=Infinity-1;
+// for(let i = 0; i < arr.length; i++){
+//     if(arr[i] > first){
+//         seclast=first
+//         first=arr[i];
+//     }else if(arr[i] > seclast && arr[i] !== first){
+//         seclast=arr[i];
+//     }
+// }
+// console.log(seclast);
+
+// let num = 10;
+// for(let i =0; i < num ; i++ ){
+//     console.log(i);
+    
+// }
+
+
+// let num = 20;
+// for(let i =0; i < num ; i++){
+//     console.log(i);
+    
+// }
+
+// let arr=[10,39,10,39,20]
+// for(let i =0; i< arr.length; i++){
+//     console.log(arr[i]);
+    
+// }
+
+function num(arr){
+    let total = 0;
+    for(let i =0; i < arr.length; i++){
+        total += arr[i];
     }
+    return total;
 }
-console.log(sclargest);
+console.log(num([10,39,29]));
