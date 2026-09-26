3e3f84bb316b37e6a759ddc99c924fbfd2047ca1
Lalbabu-Coder <rishisingh765453@gmail.com>
2026-09-26 16:26:20 +0530

    swaping of two number using third variable

--- Day-6/index.js
+++ Day-6/index.js
@@ -5,9 +5,19 @@
 // }
 
 // use while loop to print 1 to 10 numbers
-let num = 10;
- let i = 1;
- while (i <= num){
-    console.log(i);
-    i++;
- }
+// let num = 10;
+//  let i = 1;
+//  while (i <= num){
+//     console.log(i);
+//     i++;
+//  }
+
+//  swape the two numbers using third variable
+ let a = 5;
+ let b = 10;
+ let temp;
+ temp = a ;
+   a=b;
+   b = temp;
+   console.log("after swapping a = " + a);
+   console.log("after swapping b = " +b);
