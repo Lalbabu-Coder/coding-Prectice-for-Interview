76f2221bba47d251f63bf9989957653f3770c711
Lalbabu-Coder <rishisingh765453@gmail.com>
2026-09-26 14:52:57 +0530

    using while loop print the  number

---
+++ Day-6/index.js
@@ -0,0 +1,13 @@
+// //  use for loop to print 1 to 10 numbers
+// let num = 10;
+// for (let i =1 ; i <= num ; i++){
+//     console.log(i);
+// }
+
+// use while loop to print 1 to 10 numbers
+let num = 10;
+ let i = 1;
+ while (i <= num){
+    console.log(i);
+    i++;
+ }
